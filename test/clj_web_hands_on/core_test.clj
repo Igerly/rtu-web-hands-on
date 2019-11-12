86e54920350b05319d13004e426f60ba1d6d06cf
Iger <Iger@10.14.5.53>
2019-11-12 16:51:23 +0200

    - Initial commit

---
+++ test/clj_web_hands_on/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-web-hands-on.core-test
+  (:require [clojure.test :refer :all]
+            [clj-web-hands-on.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
